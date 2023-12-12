06f1c73653e512b83052bf9740b3bb25767364d1
sakib3-dev <sakib3.dev@gmail.com>
2023-12-12 21:46:03 +0600

    coral commit

--- src/pages/home/Category.jsx
+++ src/pages/home/Category.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import Flex from "../../components/Flex";
 import { Link } from "react-router-dom";
+import image1 from "../../../public/images/category/image1.png"
+import image2 from "../../../public/images/category/image2.png"
+import image3 from "../../../public/images/category/image3.png"
+import image4 from "../../../public/images/category/image4.png"
+import image5 from "../../../public/images/category/image5.png"
+import brand1 from "../../../public/images/company/brand1.png"
+import brand2 from "../../../public/images/company/brand2.png"
+import brand3 from "../../../public/images/company/brand3.png"
+import brand4 from "../../../public/images/company/brand4.png"
+import brand5 from "../../../public/images/company/brand5.png"
 
 const Category = () => {
-  const companyLogo = [
-    { id: 1, img: "/public/images/company/brand1.png" },
-    { id: 2, img: "/public/images/company/brand2.png" },
-    { id: 3, img: "/public/images/company/brand3.png" },
-    { id: 4, img: "/public/images/company/brand4.png" },
-    { id: 5, img: "/public/images/company/brand5.png" },
-  ];
   return (
     <div className="max-w-screen-2xl mx-auto container xl:px-28 py-28">
       {/* brand logo */}
       <Flex className="items-center justify-around flex-wrap gap-4 py-5">
-        {companyLogo.map(({ id, img }) => (
-          <div key={id}>
-            <img src={img} alt="brand" />
+        
+          <div>
+            <img src={brand1} alt="brand" />
           </div>
-        ))}
+          <div>
+            <img src={brand2} alt="brand" />
+          </div>
+          <div>
+            <img src={brand3} alt="brand" />
+          </div>
+          <div>
+            <img src={brand4} alt="brand" />
+          </div>
+          <div>
+            <img src={brand5} alt="brand" />
+          </div>
+        
       </Flex>
       {/* brand logo */}
 
@@ -34,7 +49,7 @@ const Category = () => {
 
         <div>
           <Link to="/">
-            <img src="/public/images/category/image1.png" alt="model" className="w-full hover:scale-105 transition-all duration-300" />
+            <img src={image1} alt="model" className="w-full hover:scale-105 transition-all duration-300" />
           </Link>
         </div>
 
@@ -44,16 +59,16 @@ const Category = () => {
         <div className="md:w-1/2">
           <div className="grid grid-cols-2 gap-2">
             <Link to="/">
-              <img src="/public/images/category/image2.png" alt="model" className="w-full hover:scale-105 transition-all duration-300"/>
+              <img src={image2} alt="model" className="w-full hover:scale-105 transition-all duration-300"/>
             </Link>
             <Link to="/">
-              <img src="/public/images/category/image3.png" alt="model" className="w-full hover:scale-105 transition-all duration-300"/>
+              <img src={image3} alt="model" className="w-full hover:scale-105 transition-all duration-300"/>
             </Link>
             <Link to="/">
-              <img src="/public/images/category/image4.png" alt="model" className="w-full hover:scale-105 transition-all duration-300"/>
+              <img src={image4} alt="model" className="w-full hover:scale-105 transition-all duration-300"/>
             </Link>
             <Link to="/">
-              <img src="/public/images/category/image5.png" alt="model" className="w-full hover:scale-105 transition-all duration-300"/>
+              <img src={image5} alt="model" className="w-full hover:scale-105 transition-all duration-300"/>
             </Link>
           </div>
         </div>
